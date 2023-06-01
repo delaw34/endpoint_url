ca65183aa2d200ec7e194ab4105b9ced7f92ba92
Lawrence Akpan <delaw34@yahoo.com>
2023-06-01 21:21:20 +0100

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,34 +1,79 @@
 import requests
-from PyPDF2 import PdfWriter
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-# Replace 'YOUR_ENDPOINT_URL' with the actual endpoint URL
-endpoint_url = 'https://api.stackexchange.com/2.3/questions?site=stackoverflow&pagesize=50&sort=votes'
+firstUrl = input('Input Stack Overflow API link: ')
+secondUrl = "https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
 
-# Send a GET request to the endpoint to fetch the questions
-response = requests.get(endpoint_url)
+user_name = input("enter user name: ")
+smtp_username = "enter smtp user name "
+smtp_password = "enter smtp password"
+sender_email = input("enter sender email: ")
+receiver_email =input("enter recipient email: ")
+subject = "Stack Overflow Results"
 
-if response.status_code == 200:
-    # Assuming the response is in JSON format
-    data = response.json()
+# Set the proper header to be sent
+try:
+    response = requests.get(firstUrl).json()
+except:
+    response = requests.get(secondUrl).json()
 
-    if 'questions' in data:
-        questions = data['questions'][:50]  # Fetch the first 50 questions
+responseLength = len(response['items'])
+header = f"DATA FETCHED FROM STACK-OVERFLOW API CONTAINING: {responseLength} RESULTS."
 
-        # Create a PDF writer object
-        pdf_writer = PdfWriter()
+def format_results():
+    results = []
+    count = 1
 
-        # Generate the PDF with numbered questions
-        for i, question in enumerate(questions, 1):
-            pdf_writer.add_page()
-            pdf_writer.drawString(10, 800, f'Question {i}: {question}')
+    # Add header
+    results.append(header)
+    results.append('\n')
 
-        # Save the PDF to a file
-        output_filename = 'questions.pdf'
-        with open(output_filename, 'wb') as output_file:
-            pdf_writer.write(output_file)
+    while count <= responseLength:
+        # Format the results
+        result = f"Number {count}\n"
+        result += f"This question was asked by: {response['items'][count-1]['owner']['display_name']} with user ID: {response['items'][0]['owner']['user_id']}\n"
+        result += f"The title of the question is: {response['items'][count-1]['title']}\n"
+        result += f"The number of views on the question is: {response['items'][count-1]['view_count']}\n"
+        result += f"The link to the question is: {response['items'][count-1]['link']}\n"
+        result += f"The link to {response['items'][count-1]['owner']['display_name']}'s profile image is: {response['items'][count-1]['owner']['profile_image']}\n"
+        result += '\n'
+        results.append(result)
+        count += 1
 
-        print(f'Successfully saved {len(questions)} questions in "{output_filename}".')
-    else:
-        print('The API response does not contain a "questions" key.')
-else:
-    print('Failed to fetch questions. Please check your endpoint URL.')
+        # Return only 50 results
+        if count == 51:
+            break
+
+    return '\n'.join(results)
+
+# Format the results
+formatted_results = format_results()
+
+# Email configuration
+
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+
+# Create a plain text version of the email
+plain_text = MIMEText(formatted_results, "plain")
+
+# Attach the plain text version to the email
+message.attach(plain_text)
+
+# Send the email
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+
+
+try:
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+    print(user_name, "Questions downloaded and sent via email successfully.")
+except Exception as e:
+    print(user_name, "An error occurred while sending the email:", str(e)) 
